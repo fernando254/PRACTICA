5ffcb551273a1cc32df9409548b3d71ce00b0ea0
Fernando Orellana <82738201+fernando254@users.noreply.github.com>
2022-10-02 15:03:20 -0300

    nuevos cambio templates

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 #importar los "views" desde archivos views.py
-from .views import home, inicio_seccion
+from .views import cerrar_seccion, home, inicio, juegos, recomendacion, seguimiento
 
 
 urlpatterns = [
     
     path('',home,name='HOME'),
-    path('inicio_seccion/',inicio_seccion,name='INICIO'),
+    path('inicio/',inicio,name='INICIO'),
+    path('juegos/',juegos,name='JUEGO'),
+    path('recomendacion/',recomendacion,name='RECOMENDACION'),
+    path('seguimiento/',seguimiento,name='SEGUIMIENTO'),
+    path('cerrar_sesion/',cerrar_seccion,name='CERRAR'),
+    
+    
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
 
+# importar la tablas USER desde el administrador
+from django.contrib.auth.models import User
+
+# importar librerias de validacion de login de acceso
+from django.contrib.auth import authenticate, logout, login
+
+#agregar libreria de decoradores para limitar el ingreso
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
-def inicio_seccion(request):
-    return render(request, "inicio_seccion.html")
+def inicio(request):
+    mensaje=''
+    if request.POST:
+        usuario = request.POST.get("nom_user")
+        contra = request.POST.get("pass")
+        us = authenticate(request, username=usuario, password=contra)
+        # el usuario no esta vacio y ademas el usuario activo
+        if us is not None and us.is_active:
+            login(request,us)
+            # ir al home
+            return render(request, "home.html")
+        else:
+            mensaje = "usuario o contraseña incorrecta"
+    datos = {"mensaje":mensaje}
+    return render(request, "inicio.html", datos)
+
+def juegos(request):
+    return render(request, "juegos.html")
+
+def recomendacion(request):
+    return render(request, "recomendacion.html")
+
+def seguimiento(request):
+    return render(request, "seguimiento.html")
+
+def cerrar_seccion(request):
+    logout(request)
+    return render(request, "home.html")
